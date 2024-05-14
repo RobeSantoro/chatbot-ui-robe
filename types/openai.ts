d283b60fea6b2a04a4ed2fbc2a227ed6ba205768
RobeSantoro <santoro.robe@gmail.com>
2024-05-15 00:26:29 +0200

    Updated with GPT-4o, sorted models

--- types/openai.ts
+++ types/openai.ts
@@ -8,60 +8,46 @@ export interface OpenAIModel {
 }
 
 export enum OpenAIModelID {
-  GPT_3_5 = 'gpt-3.5-turbo',
-  GPT_3_5_AZ = 'gpt-35-turbo',
+  GPT_4O = 'gpt-4o',
+  GPT_4_TURBO = 'gpt-4-turbo',
   GPT_4 = 'gpt-4',
-  GPT_4_32K = 'gpt-4-32k',
-  GPT_4_1106_PREVIEW = 'gpt-4-1106-preview',
-  GPT_4_0125_PREVIEW = 'gpt-4-0125-preview', // gpt-4-turbo-preview currently points to gpt-4-0125-preview.
-  GPT_4_TURBO_2024_04_09 = 'gpt-4-turbo-2024-04-09', // gpt-4-turbo currently points to gpt-4-turbo-2024-04-09.
+  GPT_3_5_TURBO = 'gpt-3.5-turbo',
+  GPT_3_5 = 'gpt-3.5',
 }
 
 // in case the `DEFAULT_MODEL` environment variable is not set or set to an unsupported model
 export const fallbackModelID = OpenAIModelID.GPT_3_5;
 
 export const OpenAIModels: Record<OpenAIModelID, OpenAIModel> = {
-  [OpenAIModelID.GPT_3_5]: {
-    id: OpenAIModelID.GPT_3_5,
-    name: 'GPT-3.5',
-    maxLength: 12000,
-    tokenLimit: 4000,
+  [OpenAIModelID.GPT_4O]: {
+    id: OpenAIModelID.GPT_4O,
+    name: 'GPT-4O',
+    maxLength: 4096,
+    tokenLimit: 128000,
   },
-  [OpenAIModelID.GPT_3_5_AZ]: {
-    id: OpenAIModelID.GPT_3_5_AZ,
-    name: 'GPT-3.5',
-    maxLength: 12000,
-    tokenLimit: 4000,
+  [OpenAIModelID.GPT_4_TURBO]: {
+    id: OpenAIModelID.GPT_4_TURBO,
+    name: 'GPT-4 Turbo',
+    maxLength: 4096,
+    tokenLimit: 128000,
   },
   [OpenAIModelID.GPT_4]: {
     id: OpenAIModelID.GPT_4,
     name: 'GPT-4',
-    maxLength: 24000,
-    tokenLimit: 8000,
-  },
-  [OpenAIModelID.GPT_4_32K]: {
-    id: OpenAIModelID.GPT_4_32K,
-    name: 'GPT-4-32K',
-    maxLength: 96000,
-    tokenLimit: 32000,
-  },
-  [OpenAIModelID.GPT_4_1106_PREVIEW]: {
-    id: OpenAIModelID.GPT_4_1106_PREVIEW,
-    name: 'GPT-4-1106-PREVIEW',
     maxLength: 4096,
-    tokenLimit: 128000,
+    tokenLimit: 8192,
   },
-  [OpenAIModelID.GPT_4_0125_PREVIEW]: {
-    id: OpenAIModelID.GPT_4_0125_PREVIEW,
-    name: 'GPT-4-0125-PREVIEW',
+  [OpenAIModelID.GPT_3_5_TURBO]: {
+    id: OpenAIModelID.GPT_3_5_TURBO,
+    name: 'GPT-3.5 Turbo',
     maxLength: 4096,
-    tokenLimit: 128000,
+    tokenLimit: 16385,
   },
-  [OpenAIModelID.GPT_4_TURBO_2024_04_09]: {
-    id: OpenAIModelID.GPT_4_TURBO_2024_04_09,
-    name: 'GPT-4-TURBO-2024-04-09',
+  [OpenAIModelID.GPT_3_5]: {
+    id: OpenAIModelID.GPT_3_5,
+    name: 'GPT-3.5',
     maxLength: 4096,
-    tokenLimit: 128000,
+    tokenLimit: 4096,
   },
 
 };
